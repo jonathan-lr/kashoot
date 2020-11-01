0072c8d53c30a24b430a6efa85b1a5d914294915
Jonathan <jonathan.cox@lumen-research.com>
2020-11-01 03:14:37 +0000

    Create component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "./Home/Home";
 import Room from "./Room/Room";
+import Create from "./Create/create";
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
             <div className="App">
                 <header className="App-header">
                     <Switch>
+                        <Route path="/create" component={Create}/>
                         <Route path="/play" component={Room} />
                         <Route path="/" component={Home} />
                     </Switch>
